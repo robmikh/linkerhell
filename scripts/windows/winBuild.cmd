a1b98eda3ae7e28b162a3009bbdc02b010872006
Robert Mikhayelyan <rob.mikh@outlook.com>
2019-08-05 19:52:47 -0700

    add release and wgpu dependencies

--- scripts/windows/winBuild.cmd
+++ scripts/windows/winBuild.cmd
@@ -1,6 +1,13 @@
 @echo off
 setlocal
 
+set CMAKE_BUILD_TYPE=Debug
+set CARGO_BUILD_FLAG=
+if "%1"=="release" (
+    set CMAKE_BUILD_TYPE=Release
+    set CARGO_BUILD_FLAG="--release"
+) 
+
 pushd %~dp0..\..\
 set "REPO_ROOT_PATH=%CD%"
 popd
@@ -9,8 +16,8 @@ set BUILDPATH=%REPO_ROOT_PATH%\_build\windows
 
 pushd %REPO_ROOT_PATH%\rust-lib
 set CARGO_TARGET_DIR=%BUILDPATH%\rust-lib
-cargo build --features dx12
+cargo build --features dx12 %CARGO_BUILD_FLAG%
 popd
 
-cmake . "-B%BUILDPATH%" -GNinja -DCMAKE_BUILD_TYPE=Debug "-DCMAKE_INSTALL_PREFIX=%BUILDPATH%" 
+cmake . "-B%BUILDPATH%" -GNinja "-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%" "-DCMAKE_INSTALL_PREFIX=%BUILDPATH%" 
 ninja -C "%BUILDPATH%" install
