f228eed462e8fcc7ec288e5a704532d8f24f1431
Robert Mikhayelyan <rob.mikh@outlook.com>
2019-08-05 20:08:39 -0700

    clear to green

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -29,24 +29,61 @@ pub unsafe extern "C" fn rust_lib_main() {
     };
     let mut swap_chain = device.create_swap_chain(&surface, &swap_chain_description);
 
-    events_loop.run_forever(|event| {
-        match event {
+    let mut running = true;
+    while running {
+        let mut new_size: Option<winit::dpi::LogicalSize> = None;
+        events_loop.poll_events(|event| {
+            match event {
                 winit::Event::WindowEvent {
-                event: winit::WindowEvent::CloseRequested,
-                ..
-            } => winit::ControlFlow::Break,
-            winit::Event::WindowEvent {
-                event: winit::WindowEvent::Resized(_),
-                ..
-            } => {
-                let dpi_factor = window.get_hidpi_factor();
-                let size = window.get_inner_size().unwrap().to_physical(dpi_factor);
+                    event: winit::WindowEvent::CloseRequested,
+                    ..
+                } => running = false,
+                winit::Event::WindowEvent {
+                    event: winit::WindowEvent::Resized(size),
+                    ..
+                } => new_size = Some(size),
+                _ => {},
+            }
+        });
 
-                println!("Window resized! {} x {}", size.width as f32, size.height as f32);
-
-                winit::ControlFlow::Continue
-            },
-            _ => winit::ControlFlow::Continue,
+        if !running {
+            break;
         }
-    });
+
+        if let Some(size) = new_size {
+            let dpi_factor = window.get_hidpi_factor();
+            let size = size.to_physical(dpi_factor);
+
+            println!("Window resized! {} x {}", size.width as f32, size.height as f32);
+
+            let mut dpi_factor = window.get_hidpi_factor();
+            let mut size = window.get_inner_size().unwrap().to_physical(dpi_factor);
+            let mut swap_chain_description = wgpu::SwapChainDescriptor {
+                usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+                format: wgpu::TextureFormat::Bgra8Unorm,
+                width: size.width as u32,
+                height: size.height as u32,
+                present_mode: wgpu::PresentMode::Vsync,
+            };
+            swap_chain = device.create_swap_chain(&surface, &swap_chain_description);
+        }
+
+        let frame = swap_chain.get_next_texture();
+        let mut encoder =
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 });
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    load_op: wgpu::LoadOp::Clear,
+                    store_op: wgpu::StoreOp::Store,
+                    clear_color: wgpu::Color::GREEN,
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        device.get_queue().submit(&[encoder.finish()]);
+    }
 }
