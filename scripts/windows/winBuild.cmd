2ec72c4888b425d0c4424343b044975c938dbdc2
Robert Mikhayelyan <rob.mikh@outlook.com>
2019-08-06 20:09:15 -0700

    add vulkan support on windows

--- scripts/windows/winBuild.cmd
+++ scripts/windows/winBuild.cmd
@@ -3,10 +3,14 @@ setlocal
 
 set CMAKE_BUILD_TYPE=Debug
 set CARGO_BUILD_FLAG=
+set WGPU_FEATURE=dx12
 if "%1"=="release" (
     set CMAKE_BUILD_TYPE=Release
     set CARGO_BUILD_FLAG="--release"
 ) 
+if "%2"=="vulkan" (
+    set WGPU_FEATURE=vulkan
+)
 
 pushd %~dp0..\..\
 set "REPO_ROOT_PATH=%CD%"
@@ -16,7 +20,7 @@ set BUILDPATH=%REPO_ROOT_PATH%\_build\windows
 
 pushd %REPO_ROOT_PATH%\rust-lib
 set CARGO_TARGET_DIR=%BUILDPATH%\rust-lib
-cargo build --features dx12 %CARGO_BUILD_FLAG%
+cargo build --features %WGPU_FEATURE% %CARGO_BUILD_FLAG%
 popd
 
 cmake . "-B%BUILDPATH%" -GNinja "-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%" "-DCMAKE_INSTALL_PREFIX=%BUILDPATH%" 
